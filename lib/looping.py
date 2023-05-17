f86cdb93ac6e08e507d1c8af93e558478bf5573b
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-17 22:06:34 +0300

    completed assignments

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    num = 10
+    while num > 0 :
+        print (num)
+        num -= 1
+    print("Happy New Year!")
+    
 def square_integers(int_list):
-    # code goes here!
-    pass
+    #use list comprehension
+    squared_num =[num **2  for num in int_list]
+    return squared_num
+#or use append 
+#def square_integers("int_list")
+#squared_num =[]
+#for num in int_list:
+#squared_num.append(num **2)
+#return squared_num
+
+    
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range (1, 101):
+        if num % 3 == 0 and num % 5 ==0:
+            print ("FizzBuzz")
+        elif num % 3 == 0:
+            print ("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+          
